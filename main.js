0ac8b66531b9d052027cf9ea3b4b3a751c934a51
mamoudou <konemamoudou84@gmail.com>
2023-04-29 23:30:20 +0200

    fin premier tp Yvan :fire:

--- main.js
+++ main.js
@@ -1,44 +1,96 @@
-/**
-   * Crée un élément du dom, lui ajoute du texte, le place comme dernier
-   * enfant de parent et ajoute un attribut en utilisant le paramètre attribute
-   * @param {String} markup_name 
-   * @param {String} text 
-   * @param {domElement} parent 
-   * @param {Array} attributes  (doit comprendre les propriétés name et value)
-   * @returns domElement
-   */
 
-
-// Création des éléments du  DOM via la fonction createMarkup
 const header = createMarkup('header', "", document.body);
 const nav = createMarkup('nav', '', header);
-let limitButton = 4;
+const container = createMarkup('div', '', document.body,[{name: 'class', value:'container'}]);
+const textMenu = ['all','html', 'css', 'js']
+const text = ['html', 'css', 'js']
 let limitCard = 16;
+let tab = [];
 
-for (let i = 0; i < limitButton; i++) {
-    const text = ['all', 'html','css','js']
+
+
+
+
+// To create button menu 
+for (let i = 0; i < textMenu.length; i++) {
     createMarkup('input','', nav,
         [
             {name:'type',value: 'button'},
-            {name: 'value', value: text[i]}
+            { name: 'class', value: 'menu' },
+            {name: 'value', value:textMenu[i]},
         ]
     );
 }
 
-for(let i = 0; i < limitCard; i++){
 
-    const text = ['all', 'html','css','js']
-    createMarkup('div', '', nav,
-        [
-            { name: 'type', value: 'button' },
-            { name: 'value', value: text[i] }
-        ]
-    );
+// To fill tab of text h2
+for (let j = 0; j < 6 ; j++) {
+
+    for (let k = 0; k < text.length; k++) {
+        tab.push(text[k])
+    }
 }
+
+
+
+
+// To create card
+
+for(let m = 0; m < limitCard; m++){
+    const card = createMarkup('div','', container,
+            [
+                { name: 'class', value: 'card ' + tab[m] },
+                { name: 'style', value:'width: 18rem'}
+            ]
+        );
+    const cardBody = createMarkup('div','',card,[{name:'class', value:'card-body'}]);
+    createMarkup('h2',tab[m],cardBody)
+    createMarkup('p',`Some quick example text to build on the card title and make up the bulk of the card's
+                    content.`, cardBody, [{ name: 'class', value:'card-text'}]);
+    createMarkup('a', 'Learn', cardBody, [{ name: 'class', value:'btn btn-primary'}])
+
+}
+
+// To create filter cards
+const buttons = document.querySelectorAll('.menu');
+const cards = document.querySelectorAll('.card')
+
+buttons.forEach((elt)=> elt.addEventListener('click',(e)=>{
+    console.log(e.target.value)
+    const clickValue = e.target.value
+    cards.forEach((card)=>{
+    
+        if(clickValue === 'all'){
+            card.style.display = 'block'
+        }else{
+            if (card.classList.contains(clickValue)) {
+                card.style.display = 'block'
+            } else {
+                card.style.display = 'none'
+            }
+        }
+
+
+    })
+
+}))
+
+
+
+/**
+   * Crée un élément du dom, lui ajoute du texte, le place comme dernier
+   * enfant de parent et ajoute un attribut en utilisant le paramètre attribute
+   * @param {String} markup_name
+   * @param {String} text
+   * @param {domElement} parent
+   * @param {Array} attributes  (doit comprendre les propriétés name et value)
+   * @returns domElement
+   */
+// Création des éléments du  DOM via la fonction createMarkup
 
 function createMarkup(markup_name, text, parent, attributes = []) {
     const markup = document.createElement(markup_name);
-    markup.textContent = text;
+    markup.innerHTML = text;
     parent.appendChild(markup);
     attributes.forEach((attribute) => {
         if (attribute && attribute.hasOwnProperty("name") && attribute.hasOwnProperty("value")) {
